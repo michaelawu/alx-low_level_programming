b27cb8bb90b5e37c33ac060526c8a1e4d36aa946
michaelawu <michaelawu230@gmail.com>
2023-03-20 05:56:31 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - writting a new program followed by printing exactly a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
