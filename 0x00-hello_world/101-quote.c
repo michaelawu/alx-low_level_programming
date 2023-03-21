a068c81d2411d71f362280c089af6c1a3e2cc13b
michaelawu <michaelawu230@gmail.com>
2023-03-20 18:52:41 -0700

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd,h>
 /**
- * main - A C program that prints a line to the standard error
+ * main - A C program that prints exactly a new line to standard error
  * Return 1 (success)
  */
-int main(void)
+ int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-       return (1);
-}       
+        return (1);
+}	
