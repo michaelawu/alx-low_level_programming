2d551343f3a215fc1757eda3e1371e5de3a735a4
michaelawu <michaelawu230@gmail.com>
2023-03-24 21:46:36 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void);
+
+{
+    print_numbers(98);
+    _putchar('\n');
+    print_numbers(402);
+    _putchar('\n');
+    print_numbers(1024);
+    _putchar('\n');
+    print_numbers(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition to the multiplication
+ */
+
+int mul(int a, int b);
+{
+	Return: (a * b);
+}
